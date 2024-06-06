a0b922dfddc1e8c5a77ed27afed6bbb323dc3e31
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-07 04:40:19 +0530

    🆕Feat/Created Loaders

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
+import {  Backdrop } from '@mui/material'; 
+import { LayoutLoader } from "./components/layout/Loaders";
+
 
 //DYNAMIC LOADING
 const Home = lazy(() => import("./pages/Home"));
@@ -14,17 +17,19 @@ const user = true;
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<ProtectRoute user={user} />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/groups" element={<Groups />} />
-        </Route>
-        <Route element={<ProtectRoute user={!user} />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<LayoutLoader/>}>
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+          <Route element={<ProtectRoute user={!user} />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
