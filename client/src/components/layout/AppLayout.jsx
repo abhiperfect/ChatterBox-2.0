f8f62640333cb8d16078e5ede9fda1b4569ad448
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-06-07 14:52:00 +0530

    🐛Fix/undefined object error in chat.

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -31,7 +31,7 @@ const AppLayout = () => (WrappedComponent) => {
             />
           </Grid>
           <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
-            <WrappedComponent {...props} />
+          <WrappedComponent {...props} chatId={"1"} user={"user"} />
           </Grid>
 
           <Grid
